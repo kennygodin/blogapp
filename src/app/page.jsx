256578446fb091d89d31e6eb7b46147191b1576e
kennycodin <kenechukwuokoh30@gmail.com>
2023-10-07 17:22:38 +0100

    Inital commit

---
+++ src/app/page.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import styles from './homepage.module.css';
+
+export default function Home() {
+  return (
+    <div>
+      <Link href="#">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
+        distinctio impedit tenetur aspernatur sapiente, cum animi commodi id
+        suscipit itaque, assumenda voluptate autem velit eum labore. Animi
+        voluptatum ex nisi.
+      </Link>
+    </div>
+  );
+}
